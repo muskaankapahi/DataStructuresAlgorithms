678ccee092f20e872a16679f2c2f9a447714451a
Muskaan Kapahi <49089408+muskaankapahi@users.noreply.github.com>
2024-06-28 22:27:36 -0700

    Update Queues.java

--- Queues.java
+++ Queues.java
@@ -11,18 +11,21 @@ import java.util.LinkedList;
 
 public class Queues{
     public static void queues_DS(String[] args){
-
+        //  Initialize a queue.
         Queue<String> queue = new LinkedList<String>();
-
+        //  Push new element.
         queue.offer("Karen");
         queue.offer("Chad");
         queue.offer("Steve");
         queue.offer("Harold");
 
         System.out.println(queue.isEmpty());
+         //  Get the size of the queue.
         System.out.println(queue.size());
         System.out.println(queue.contains("Chad"));
+         //  Get the first element - return null if queue is empty.
         System.out.println(queue.peek());
+        //  Pop an element.
         System.out.println(queue.poll());
         System.out.println(queue);
 
