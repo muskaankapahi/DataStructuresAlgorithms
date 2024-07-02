cc289c590e0832b1da0f2eccd19aa3f2b30797e0
Muskaan Kapahi <muskaankapahi@gmail.com>
2024-07-01 21:59:45 -0700

    Practice bs

--- FirstNotSmaller.java
+++ FirstNotSmaller.java
@@ -1,3 +1,8 @@
+/* Given an array of integers sorted in increasing order and a target,
+find the index of the first element in the array that is larger than
+or equal to the target. Assume that it is guaranteed to find a satisfying number.
+
+ */
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
---
+++ FirstOccurenceWithDuplicates.java
@@ -0,0 +1,50 @@
+/* Given a sorted array of integers and a target integer, find the first
+occurrence of the target and return its index. Return -1 if the target is not in the array.
+
+Input:
+arr = [1, 3, 3, 3, 3, 6, 10, 10, 10, 100]
+target = 3
+Output: 1
+* */
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+class Solution {
+    public static int findFirstOccurrence(List<Integer> arr, int target) {
+        // WRITE YOUR BRILLIANT CODE HERE
+        int start = 0 , end = arr.size() - 1;
+        int boundary = -1;
+        while(start <= end)
+        {
+            int mid = start + (end - start) / 2;
+    
+            if(arr.get(mid) == target)
+            {
+                boundary = mid;
+                end = mid - 1;
+            } else if (arr.get(mid) < target) {
+                start = mid + 1;
+            }
+            else{
+                end = mid -1;
+            }
+        }
+        return boundary;
+    }
+
+    public static List<String> splitWords(String s) {
+        return s.isEmpty() ? List.of() : Arrays.asList(s.split(" "));
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        List<Integer> arr = splitWords(scanner.nextLine()).stream().map(Integer::parseInt).collect(Collectors.toList());
+        int target = Integer.parseInt(scanner.nextLine());
+        scanner.close();
+        int res = findFirstOccurrence(arr, target);
+        System.out.println(res);
+    }
+}
+
